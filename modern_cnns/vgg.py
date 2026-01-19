1055a2f6cee2be3cd66f4dae655444094e592a95
Samuel He <samuelhe52@outlook.com>
2026-01-19 19:05:05 +0800

    Add VGG; Use new TrainingConfig API for AlexNet and VGG

--- modern_cnns/vgg.py
+++ modern_cnns/vgg.py
@@ -4,35 +4,38 @@ from utils.classfication import train, fashion_mnist
 from utils import TrainingLogger
 from utils.training_config import TrainingConfig
 
-class AlexNet(nn.Module):
-    def __init__(self, num_classes=10):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(1, 96, kernel_size=11, stride=4, padding=2), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(96, 256, kernel_size=5, padding=2), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(256, 384, kernel_size=3, padding=1), nn.ReLU(),
-            nn.Conv2d(384, 384, kernel_size=3, padding=1), nn.ReLU(),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Flatten(),
-            nn.Linear(256 * 6 * 6, 4096), nn.ReLU(),
-            nn.Dropout(), # Default dropout rate of 0.5
-            nn.Linear(4096, 4096), nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes)
-        )
+def vgg_block(num_convs, out_channels):
+    layers = []
+    for _ in range(num_convs):
+        layers.append(nn.LazyConv2d(out_channels, kernel_size=3, padding=1))
+        layers.append(nn.ReLU())
+    layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+    return nn.Sequential(*layers)
 
+class VGG(nn.Module):
+    def __init__(self, arch, num_classes=10):
+        super().__init__()
+        vgg_blks = []
+        for (num_convs, out_channels) in arch:
+            vgg_blks.append(vgg_block(num_convs, out_channels))
+        self.net = nn.Sequential(
+            *vgg_blks, nn.Flatten(),
+            nn.LazyLinear(4096), nn.ReLU(), nn.Dropout(),
+            nn.LazyLinear(4096), nn.ReLU(), nn.Dropout(),
+            nn.LazyLinear(num_classes)
+        )
+        
     def forward(self, X):
         return self.net(X)
 
 if __name__ == "__main__":
-    batch_size = 128
+    batch_size = 64
     num_epochs = 10
     lr = 0.01
     
-    model = AlexNet()
+    vgg11_arch = [(1, 64), (1, 128), (2, 256), (2, 512), (2, 512)]
+        
+    model = VGG(arch=vgg11_arch)
     dataloader = fashion_mnist(batch_size, resize=224, data_root='data/')
     val_dataloader = fashion_mnist(batch_size, train=False, resize=224, data_root='data/')
     init_fn = torch.nn.init.kaiming_uniform_
@@ -45,20 +48,21 @@ if __name__ == "__main__":
     }
     
     logger = TrainingLogger(
-        log_path='logs/alexnet_experiment.json',
+        log_path='logs/vgg_experiment.json',
         hparams=hparams
     )
     
     def init_weights(m):
         if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
             init_fn(m.weight)
-            
+    
+    model.forward(torch.randn(1, 1, 224, 224))  # Initialize lazy layers
     model.apply(init_weights)
 
     config = TrainingConfig(
         num_epochs=num_epochs,
         lr=lr,
-        save_path='models/alexnet.pt',
+        save_path='models/vgg.pt',
         logger=logger,
     )
 
